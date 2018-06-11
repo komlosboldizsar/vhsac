726cb38583c4bfa660dfaed0e48b79bd72a06d75
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-06-11 13:07:37 +0200

    Add logger

--- VHSAC/Logger/FileLogger.cs
+++ VHSAC/Logger/FileLogger.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VHSAC.Logger
 {
-    class FileLogger
+    public class FileLogger
     {
+
+        private string _fileName;
+
+        public FileLogger(string fileName)
+        {
+            _fileName = fileName;
+            Logger.Handlers += NewLogMessageHandler;
+        }
+
+        public void NewLogMessageHandler(string message)
+        {
+            using (StreamWriter sw = File.AppendText(_fileName))
+            {
+                sw.WriteLine(message);
+            }
+        }
+
     }
 }
--- VHSAC/Logger/Logger.cs
+++ VHSAC/Logger/Logger.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace VHSAC.Logger
 {
-    class Logger
+    public class Logger
     {
+
+        public delegate void NewLogMessageHandler(string message);
+        public static event NewLogMessageHandler Handlers;
+
+        public static void Log(string message)
+        {
+            Handlers?.Invoke(message);
+        }
+
     }
 }
