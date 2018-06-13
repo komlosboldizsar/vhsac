1ff6394825b0ff778e869b0100d306dbeabf3940
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-06-13 20:04:48 +0200

    Add batch manager

---
+++ VHSAC/Model/VTR/BatchManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VHSAC.Model.VTR
+{
+    class BatchManager
+    {
+
+        public static void Start()
+        {
+
+            // Check states
+            foreach (VTR vtr in Program.VTRs)
+                if (vtr.UseInNextBatch || (vtr.State != VTRState.Reset))
+                    throw new Exception(/* TODO */);
+
+            startNextVTR();
+
+        }
+
+        public static void Stop()
+        {
+            currentVTR.StateChanged -= currentVTRStateChangedHandler;
+            foreach (VTR vtr in Program.VTRs)
+                if(vtr.State == VTRState.Capturing)
+                    vtr.StopCapture();
+        }
+
+        public static void ResetAll()
+        {
+
+            // Check states
+            foreach (VTR vtr in Program.VTRs)
+                if ((vtr.State == VTRState.Starting) || (vtr.State == VTRState.Capturing) || (vtr.State == VTRState.Stopping))
+                    throw new Exception(/* TODO */);
+
+            // Reset all
+            foreach (VTR vtr in Program.VTRs)
+                vtr.Reset();
+
+        }
+
+        private static VTR currentVTR;
+
+        private static void startNextVTR()
+        {
+            getNextUsableVTR();
+            if(currentVTR != null)
+            {
+                currentVTR.StateChanged += currentVTRStateChangedHandler;
+                currentVTR.StartCapture();
+            }
+        }
+
+        private static void currentVTRStateChangedHandler(VTR vtr, VTRState newState)
+        {
+            if((newState == VTRState.ManuallyStopped) || (newState == VTRState.ManuallyStopped) || (newState == VTRState.Failure))
+            {
+                currentVTR.StateChanged -= currentVTRStateChangedHandler;
+                startNextVTR();
+            }
+        }
+
+        private static void getNextUsableVTR()
+        {
+            foreach (VTR vtr in Program.VTRs)
+            {
+                if (vtr.UseInNextBatch || (vtr.State == VTRState.Reset))
+                {
+                    currentVTR = vtr;
+                    return;
+                }
+            }
+            currentVTR = null;
+        }
+        
+
+    }
+}
