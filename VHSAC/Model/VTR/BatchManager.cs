70bec1803eff84d459cfb1022d5bd821f6356985
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-06-14 08:34:55 +0200

    Add properties to BatchManager that contain if certain operations are enabled

--- VHSAC/Model/VTR/BatchManager.cs
+++ VHSAC/Model/VTR/BatchManager.cs
@@ -9,6 +9,12 @@ namespace VHSAC.Model.VTR
     class BatchManager
     {
 
+        #region Public methods
+        public static void Init()
+        {
+            calculateGlobalState();
+        }
+
         public static void Start()
         {
 
@@ -46,6 +52,7 @@ namespace VHSAC.Model.VTR
                 vtr.Reset();
 
         }
+        #endregion
 
         private static VTR currentVTR;
 
@@ -80,7 +87,82 @@ namespace VHSAC.Model.VTR
             }
             currentVTR = null;
         }
-        
+
+        private static void calculateGlobalState()
+        {
+
+            bool canStart = true;
+            bool canStop = true;
+            bool canReset = true;
+            foreach (VTR vtr in Program.VTRs)
+            {
+                if ((vtr.State == VTRState.Starting) || (vtr.State == VTRState.Capturing) || (vtr.State == VTRState.Stopping))
+                    canReset = false;
+                if (vtr.UseInNextBatch && (vtr.State != VTRState.Reset))
+                    canStart = false;
+            }
+
+            CanStart = canStart;
+            CanStop = canStop;
+            CanReset = canReset;
+
+        }
+
+        public delegate void OperationEnableStateChangedHandler(bool newValue);
+
+        #region Property: CanStart
+        private static bool _canStart;
+
+        public static bool CanStart
+        {
+            get { return _canStart; }
+            set
+            {
+                bool oldValue = _canStart;
+                _canStart = value;
+                if (oldValue != value)
+                    CanStartChanged?.Invoke(value);
+            }
+        }
+
+        public static event OperationEnableStateChangedHandler CanStartChanged;
+        #endregion
+
+        #region Property: CanStop
+        private static bool _canStop;
+
+        public static bool CanStop
+        {
+            get { return _canStop; }
+            set
+            {
+                bool oldValue = _canStop;
+                _canStop = value;
+                if (oldValue != value)
+                    CanStopChanged?.Invoke(value);
+            }
+        }
+
+        public static event OperationEnableStateChangedHandler CanStopChanged;
+        #endregion
+
+        #region Property: CanReset
+        private static bool _canReset;
+
+        public static bool CanReset
+        {
+            get { return _canReset; }
+            set
+            {
+                bool oldValue = _canReset;
+                _canReset = value;
+                if (oldValue != value)
+                    CanResetChanged?.Invoke(value);
+            }
+        }
+
+        public static event OperationEnableStateChangedHandler CanResetChanged;
+        #endregion
 
     }
 }
