6d1d557d375a2f07611040a9bda47b7697623f90
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-07-06 09:21:07 +0200

    Add metadata struct to model

---
+++ VHSAC/Model/Metadata/CaptureMetadata.cs
@@ -0,0 +1,9 @@
+﻿namespace VHSAC.Model.Metadata
+{
+    public class CaptureMetadata
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Minutes { get; set; }
+    }
+}
